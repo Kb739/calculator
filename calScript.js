5bd06bb09f2e9388d918827e3d8064f24ec29f2a
kunal <kunalbankoti9@gmail.com>
2022-05-19 22:44:34 +0530

    add grid generator & operator functions

--- calScript.js
+++ calScript.js
@@ -1,16 +1,76 @@
+const display = document.querySelector('#display');
+const allCharacters = ['clr', 'del', '=', '/', 1, 2, 3, '*', 4, 5, 6, '-', 7, 8, 9, '+', 0, '.', '='];
+let buffer = ['', '', ''];
+let bufferIndex = 0;
+let op = '';
 
-
-
-function calculate(a, b, op) {
-    return op(a, b);
-}
-
-const operator = {
+const operation = {
     '*': mul,
     '-': sub,
     '+': add,
-    '/': divide
+    '/': divide,
+    '=': equalsTo,
+    'clr': clear,
 };
+
+function reset() {
+    for (let i = 0; i < 3; i++)
+        buffer[i] = '';
+    bufferIndex = 0;
+}
+
+function numberInput(e) {
+    if (bufferIndex == 2) {
+        reset();
+    }
+    let num = buffer[bufferIndex];
+    if (num.length < 12) {
+        num += e.target.textContent;
+        buffer[bufferIndex] = num;
+        display.textContent = num;
+    }
+}
+function operatorInput(e) {
+    if (buffer[bufferIndex]) {
+        switch (bufferIndex) {
+
+            case 0:
+                bufferIndex++;
+                break;
+
+            case 1:
+                buffer[0] = calculate(buffer[0], buffer[1], operation[op]);
+                buffer[1] = '';
+                bufferIndex = 1;
+                display.textContent = buffer[0];
+                break;
+
+            case 2:
+                buffer[0] = buffer[2];
+                buffer[1] = '';
+                buffer[2] = '';
+                bufferIndex = 1;
+                break;
+
+        }
+        op = e.target.textContent;
+    }
+}
+
+function equalsTo() {
+    if (buffer[0] && buffer[1]) {
+        if (!buffer[2]) {
+            buffer[2] = calculate(buffer[0], buffer[1], operation[op]);
+            bufferIndex = 2;
+            display.textContent = buffer[2];
+        }
+    }
+}
+
+function clear() {
+    reset();
+    display.textContent = '';
+}
 
 function mul(a, b) {
     return a * b;
@@ -24,4 +84,39 @@ function add(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function calculate(a, b, op) {
+    return op(+a, +b);
+}
+
+function classify(button) {
+    const text = button.textContent;
+
+    if (text.match(/\.|[0-9]/)) {
+        button.classList.add('number');
+        button.onclick = numberInput;
+    }
+    else if (text.match(/\+|\-|\*|\//)) {
+        button.classList.add('operator');
+        button.onclick = operatorInput;
+    }
+    else {
+        button.onclick = operation[text];
+    }
+}
+
+function generateButtonGrid() {
+    const grid = document.querySelector('#grid');
+
+    for (const ele of allCharacters) {
+        const button = document.createElement('button');
+        button.classList.add('block');
+        button.textContent = ele;
+        classify(button);
+        grid.appendChild(button);
+    }
+
+}
+
+generateButtonGrid();
 
